bbe7d51b845e06868bf8622ce3309cf4f61b1cb1
Francois-Xavier Bonnet <francois-xavier.bonnet@centraliens.net>
2019-07-20 15:40:03 +1000

    Fixed stability issue

--- unread-count-for-outlook.js
+++ unread-count-for-outlook.js
@@ -1,29 +1,36 @@
-let favicon = new Favico({
-    bgColor: '#ffffff',
-    textColor: '#000000',
-    animation: 'none'
-});
+window.addEventListener("load", (event) => {
 
-function update_unread() {
-    let currVal = 0;
-    let highlightedElements = document.getElementsByTagName('span');
-    for (let i = 0; i < highlightedElements.length; i++) {
-        let child = highlightedElements[i].firstChild;
-        if (child && child.nodeType === 3) {
-            let text = child.data.trim();
-            if (text.length > 0 && !Number.isNaN(Number(text))) {
-                currVal = Number(text);
-                break;
+    let favicon = new Favico({
+        bgColor: '#ffffff',
+        textColor: '#000000',
+        animation: 'none'
+    });
+
+    function update_unread() {
+        let currVal = 0;
+        let highlightedElements = document.getElementsByTagName('span');
+        for (let i = 0; i < highlightedElements.length; i++) {
+            let child = highlightedElements[i].firstChild;
+            if (child && child.nodeType === 3) {
+                let text = child.data.trim();
+                if (text.length > 0 && !Number.isNaN(Number(text))) {
+                    currVal = Number(text);
+                    break;
+                }
             }
         }
-    }
-    try {
-        favicon.badge(currVal);
-    } catch (e) {
-        console.log(e.message);
         favicon.reset();
+        favicon.badge(currVal);
     }
-}
 
-console.log("Starting unread count for Outlook extension");
-window.setInterval(update_unread, 1000);
+    console.log("Starting unread count for Outlook extension");
+
+    // Not sure when the page will be ready after initial loading :(
+    setTimeout(update_unread, 2000);
+    setTimeout(update_unread, 5000);
+    setTimeout(update_unread, 10000);
+
+    // At least we should be notified when a new message arrives
+    let observer = new MutationObserver(update_unread);
+    observer.observe(document.body, {characterData: true, subtree: true});
+});

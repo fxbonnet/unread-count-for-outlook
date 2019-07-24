3fe62cc7caf406dde20fb11812c454676aba9954
Francois-Xavier Bonnet <francois-xavier.bonnet@centraliens.net>
2019-07-25 09:29:53 +1000

    Combine events, setInterval and deferred updates

--- unread-count-for-outlook.js
+++ unread-count-for-outlook.js
@@ -7,6 +7,7 @@ window.addEventListener("load", (event) => {
     });
 
     function update_unread() {
+		console.log("update_unread");
         let currVal = 0;
         let highlightedElements = document.getElementsByTagName('span');
         for (let i = 0; i < highlightedElements.length; i++) {
@@ -22,15 +23,21 @@ window.addEventListener("load", (event) => {
         favicon.reset();
         favicon.badge(currVal);
     }
+	
+	function deferred_update_unread() {
+		console.log("deferred_update_unread");
+		setTimeout(update_unread, 1000);
+	}
 
     console.log("Starting unread count for Outlook extension");
 
     // Not sure when the page will be ready after initial loading :(
     setTimeout(update_unread, 2000);
-    setTimeout(update_unread, 5000);
-    setTimeout(update_unread, 10000);
+	
+	// Backup mode if events don't work
+    setInterval(update_unread, 10000);
 
     // At least we should be notified when a new message arrives
-    let observer = new MutationObserver(update_unread);
+    let observer = new MutationObserver(deferred_update_unread);
     observer.observe(document.body, {characterData: true, subtree: true});
 });

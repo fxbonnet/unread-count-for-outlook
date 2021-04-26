989b592379751e4d4ff89d2ab616b8ab955be1b7
Francois-Xavier Bonnet <fbonnet@atlassian.com>
2021-04-26 14:42:19 +1000

    Identify unread count based on inbox icon next to it

--- unread-count-for-outlook.js
+++ unread-count-for-outlook.js
@@ -7,32 +7,47 @@ window.addEventListener("load", (event) => {
     });
 
     function update_unread() {
-        let currVal = 0;
-        let highlightedElements = document.getElementsByTagName('span');
-        for (let i = 0; i < highlightedElements.length; i++) {
-            let child = highlightedElements[i].firstChild;
+        favicon.reset();
+        // Try to find the unread count close to <i data-icon-name="Inbox"
+        let iElements = document.getElementsByTagName('i');
+        for (let i = 0; i < iElements.length; i++) {
+            let iElement = iElements[i];
+            if (iElement.attributes['data-icon-name'] && iElement.attributes['data-icon-name'].value === 'Inbox') {
+                let numericValue = findSpanWithNumber(iElement.parentElement);
+                if (numericValue)
+                    favicon.badge(numericValue);
+                return;
+            }
+        }
+        // If we did not find the icon, fallback to any <span containing a number
+        let numericValue = findSpanWithNumber(document);
+        if (numericValue)
+            favicon.badge(numericValue);
+    }
+
+    function findSpanWithNumber(element) {
+        let spanElements = element.getElementsByTagName('span');
+        for (let i = 0; i < spanElements.length; i++) {
+            let child = spanElements[i].firstChild;
             if (child && child.nodeType === 3) {
                 let text = child.data.trim();
                 if (text.length > 0 && !Number.isNaN(Number(text))) {
-                    currVal = Number(text);
-                    break;
+                    return Number(text);
                 }
             }
         }
-        favicon.reset();
-        favicon.badge(currVal);
     }
-	
-	function deferred_update_unread() {
-		setTimeout(update_unread, 1000);
-	}
+
+    function deferred_update_unread() {
+        setTimeout(update_unread, 1000);
+    }
 
     console.log("Starting unread count for Outlook extension");
 
     // Not sure when the page will be ready after initial loading :(
     setTimeout(update_unread, 2000);
-	
-	// Backup mode if events don't work
+
+    // Backup mode if events don't work
     setInterval(update_unread, 10000);
 
     // At least we should be notified when a new message arrives
